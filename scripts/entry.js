548f68df2c18749b0afb52ad39c764a3fe377b6c
opabinia <urootsugi@gmail.com>
2024-07-01 01:29:14 +0900

    change color based on movement amount

---
+++ scripts/entry.js
@@ -0,0 +1,26 @@
+Hooks.once("dragRuler.ready", (SpeedProvider) => {
+    class SwordWorld25SpeedProvider extends SpeedProvider {
+        get colors() {
+            return [
+                {id: "limited", default: 0x0080FF, name: "speeds.limited"},
+                {id: "normal", default: 0x80FF00, name: "speeds.normal"},
+                {id: "max", default: 0xFF8000, name: "speeds.max"}
+            ];
+        }
+
+        getRanges(token) {
+            const actor = token.actor;
+            const systemData = actor.system;
+
+            const ranges = [
+                {range: systemData.attributes.move.limited, color: "limited"},
+                {range: systemData.attributes.move.normal, color: "normal"},
+                {range: systemData.attributes.move.max, color: "max"},
+            ];
+
+            return ranges;
+        }
+    }
+
+    dragRuler.registerModule("drag-ruler-integration-for-sw25", SwordWorld25SpeedProvider);
+})
